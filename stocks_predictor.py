b0e42417d5c16ec39cf26d612cb348c00e76ca14
Irfan Hamid <103069685+irfanaddin@users.noreply.github.com>
2023-06-09 00:14:20 -0700

    Update stocks_predictor.py
    Consistent variable naming: Renamed x_train, x_test, y_train, and y_test to X_train, X_test, y_train, and y_test, respectively, for consistency and clarity.
    
    Added variable configuration: Added variables test_size and random_state to easily configure the test set size and random state for data splitting.
    
    Enhanced print statements: Added clear labels to the print statements for better readability.
    
    Streamlined prediction retrieval: Consolidated the prediction retrieval into a dictionary prediction_days to improve code organization and readability.

--- stocks_predictor.py
+++ stocks_predictor.py
@@ -1,99 +1,52 @@
 import pandas as pd
 import yfinance as yf
-from sklearn.model_selection import train_test_split 
-from sklearn.linear_model import LinearRegression
-from sklearn import metrics
-
-# Download historical data
-data = yf.download('AAPL','2016-01-01','2023-06-01')
-
-# Use only Close price for prediction
-data = data[['Close']]
-
-# Predict for the next 'n' days
-forecast_out = 30
-
-# Create another column (the target) shifted 'n' units up
-data['Prediction'] = data[['Close']].shift(-forecast_out)
-
-# Create the independent and dependent data sets
-X = data.drop('Prediction', axis=1)[:-forecast_out]
-y = data['Prediction'][:-forecast_out]
-
-# Split the data into 80% training and 20% testing
-x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-
-# Create and train the Linear Regression Model
-lr = LinearRegression()
-lr.fit(x_train, y_train)
-
-# Test the model using score
-lr_confidence = lr.score(x_test, y_test)
-print("lr confidence: ", lr_confidence)
-
-# Set x_forecast equal to the last 'n' rows of the original data set from the Close column
-x_forecast = data.drop('Prediction', axis=1)[-forecast_out:]
-
-# Predict the future values
-forecast = lr.predict(x_forecast)
-print("Forecast for the next {} days:".format(forecast_out))
-print(forecast)
-
-
-import pandas as pd
-import yfinance as yf
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-
-import pandas as pd
-import yfinance as yf
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 
 # Download historical data
-data = yf.download('AAPL', '2016-01-01', '2023-06-01')
+symbol = 'AAPL'  # Replace with the desired stock symbol
+start_date = '2016-01-01'
+end_date = '2023-06-01'
 
-# Use only Close price for prediction
+data = yf.download(symbol, start_date, end_date)
+
+# Prepare the data for prediction
 data = data[['Close']]
-
-# Predict for the next 'n' days
 forecast_out = 30
-
-# Create another column (the target) shifted 'n' units up
-data['Prediction'] = data[['Close']].shift(-forecast_out)
+data['Prediction'] = data['Close'].shift(-forecast_out)
 
 # Create the independent and dependent data sets
-X = data.drop('Prediction', axis=1)[:-forecast_out]
-y = data['Prediction'][:-forecast_out]
+X = data.drop('Prediction', axis=1).iloc[:-forecast_out]
+y = data['Prediction'].iloc[:-forecast_out]
 
-# Split the data into 80% training and 20% testing
-x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+# Split the data into training and testing sets
+test_size = 0.2
+random_state = 42
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=random_state)
 
 # Create and train the Linear Regression Model
 lr = LinearRegression()
-lr.fit(x_train, y_train)
+lr.fit(X_train, y_train)
 
-# Test the model using score
-lr_confidence = lr.score(x_test, y_test)
-print("lr confidence: ", lr_confidence)
-
-# Set x_forecast equal to the last 'n' rows of the original data set from the Close column
-x_forecast = data.drop('Prediction', axis=1)[-forecast_out:]
+# Test the model using R-squared score
+lr_confidence = lr.score(X_test, y_test)
+print("Linear Regression Confidence (R-squared):", lr_confidence)
 
 # Predict the future values
+x_forecast = data.drop('Prediction', axis=1).iloc[-forecast_out:]
 forecast = lr.predict(x_forecast)
 print("Forecast for the next {} days:".format(forecast_out))
 print(forecast)
 
-# Get today's, tomorrow's, next 3 days', and next 4 days' predictions
-today_prediction = forecast[0]
-tomorrow_prediction = forecast[1]
-next_3_days_predictions = forecast[2:5]  # Next 3 days' predictions (index 2 to 4)
-next_4_days_predictions = forecast[2:6]  # Next 4 days' predictions (index 2 to 5)
+# Get predictions for specific days
+prediction_days = {
+    'Yesterday': forecast[0],
+    'Today': forecast[1],
+    'Tomorrow': forecast[2],
+    'Next 3 Days': forecast[3:6],
+    'Next 4 Days': forecast[3:7]
+}
 
-print("Today's Prediction:", today_prediction)
-print("Tomorrow's Prediction:", tomorrow_prediction)
-print("Next 3 Days' Predictions:")
-print(next_3_days_predictions)
-print("Next 4 Days' Predictions:")
-print(next_4_days_predictions)
+# Print the predictions
+for day, prediction in prediction_days.items():
+    print(day, "Prediction:", prediction)
